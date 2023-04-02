4907036eed185055bb69de94048af2cc943211d1
lingeorge88 <lingeorge04@gmail.com>
2023-04-02 14:40:04 -0700

    fixed minor error in product routes' delete request

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -144,7 +144,7 @@ router.delete('/:id', async(req, res) => {
       res.status(404).json({message: 'No product found with this id!'});
       return;
     }
-    res.status(200).json(travellerData);
+    res.status(200).json(productData);
   }catch (err){
     res.status(500).json(err);
   }
